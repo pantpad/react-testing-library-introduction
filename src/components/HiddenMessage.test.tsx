952f9a656de5488679a47317ebc4fb1a2272a3ad
pantpad <deathmatchplayerp@gmail.com>
2024-05-09 19:53:44 +0200

    test: write hiddenMessage test + install deps

--- src/components/HiddenMessage.test.tsx
+++ src/components/HiddenMessage.test.tsx
@@ -1,3 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import * as React from "react";
+
+import HiddenMessage from "./HiddenMessage";
+import { expect, test } from "vitest";
+
+test("show the children when the button is pressed", () => {
+  const testMessage = "Test Message";
+  render(<HiddenMessage>{testMessage}</HiddenMessage>);
+
+  expect(screen.queryByText(testMessage)).toBeNull();
+
+  userEvent.click(screen.getByLabelText(/show/i));
+
+  expect(screen.getByText(testMessage)).toBeDefined();
+});
